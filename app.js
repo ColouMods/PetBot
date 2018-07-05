25ac47939c033bf1931d11c6e00207be9b97a3b4
DocBison <37480191+DocBison@users.noreply.github.com>
2018-07-05 15:37:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,29 @@ mention = ["You called?", "Yeah?", "What do you need?", "What's up?", "Hi."];
 sorry = ["OK.", "Good.", "You'd better be sorry.", "I forgive you."];
 
 client.on('message', message => {
+	if (message.content.match(/have a conversation with sparrow bot/i)) {
+	message.channel.send('<@424679471077916682> Hi');
+	}
+	if (message.author.bot == true) { 
+		if (message.content === "Hi.") {
+		message.channel.startTyping();
+		setTimeout(() => { 
+			message.channel.send('How are you?');
+		}, 3390);
+		message.channel.stopTyping(true);
+		//
+		} else if (message.content === "OK, I guess.") {
+		message.channel.startTyping();
+		setTimeout(() => { 
+			message.channel.send("That's nice.");
+		}, 2260);
+		message.channel.stopTyping(true);
+		} 
+}
+return;
+});
+
+client.on('message', message => {
 	
 });  
 // THIS  MUST  BE  THIS  WAY
