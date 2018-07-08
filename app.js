2493cadc605da19d12a0d5173e87e31027586b57
DocBison <37480191+DocBison@users.noreply.github.com>
2018-07-08 19:42:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -99,7 +99,6 @@ client.on('message', message => {
 					message.channel.sendMessage("✅ Mimicking <@" + mimicID + ">.");
 				} else {
 					message.channel.sendMessage("🛑 I refuse to mimic myself.");
-					//I know you're going to want to change this, just needed something to test it with
 				}
 			} else {
 				message.channel.sendMessage("❔ You need to tell me who to mention.");
