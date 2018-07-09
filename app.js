133d25cacf2af26697ad1d47adaf301e1e3e7616
DocBison <37480191+DocBison@users.noreply.github.com>
2018-07-09 17:38:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -15,8 +15,7 @@ client.on('ready', () => {
 
 //response arrays
 greet = ["Hello.", "Hi."];
-devs = ["333710766706524167", "259458435484090369"];
-//"290486859480563713"
+devs = ["333710766706524167", "290486859480563713", "259458435484090369"];
 mention = ["You called?", "Yeah?", "What do you need?", "What's up?", "Hi.", "Hey."];
 sorry = ["OK.", "Good.", "You'd better be sorry.", "I forgive you."];
 lyrics1 = [
