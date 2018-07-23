9ca489e4dc0d17313d3eeb3c876c9519b82cbcd8
DocBison <37480191+DocBison@users.noreply.github.com>
2018-07-23 16:35:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-	if ( (message.content.startsWith(prefix)) && ( (message.author.id==290486859480563713) || (message.author.id==333710766706524167) || (message.author.id==259458435484090369) ) ); {
+	if (message.content.startsWith(prefix)); {
 		const args = message.content.slice(prefix.length).trim().split(/ +/g);
 		const command = args.shift().toLowerCase();
 		if ( (command === 'mimic') && (message.channel.type != "dm") ) {
