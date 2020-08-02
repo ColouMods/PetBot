3b72263e6ba83fd35e37b95e714cf9213294b092
DocBison <37480191+DocBison@users.noreply.github.com>
2020-08-02 17:20:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ client.on('message', message => {
 					//} else {
 					//	message.channel.sendMessage("❔ You need to tell me who to mimic.");
 					//}
-				} else { message.channel.send("🛑 Can't mimic here."); }
+				} //else { message.channel.send("🛑 Can't mimic here."); }
 				break;		
 			case 'test':
 				message.channel.sendMessage("hasVoted.length is currently " + hasVoted.length);
