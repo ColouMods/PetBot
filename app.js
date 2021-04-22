8bad276afcde196948c9d282ea3a0f3a95024652
DocBison <37480191+DocBison@users.noreply.github.com>
2021-04-22 14:06:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -113,12 +113,12 @@ client.on('message', message => {
 				var ln2 = Math.floor(Math.random() * h7.length);
     				var ln3 = Math.floor(Math.random() * h5.length);
 				
-    				if (ln3 == ln1) {
-        				if (ln3 < h5.length) {
-						ln3++;
-					} else {
-						ln3--;
-				}
+    				//if (ln3 == ln1) {
+        			//	if (ln3 < h5.length) {
+				//		ln3++;
+				//	} else {
+				//		ln3--;
+				//}
 				
     				message.channel.sendMessage(h5[ln1] + "\n" + h7[ln2] + "\n" + h5[ln3]);
 				//message.channel.sendMessage(h5[Math.floor(Math.random() * h5.length)] + "\n" + h7[Math.floor(Math.random() * h7.length)] + "\n" + h5[Math.floor(Math.random() * h5.length)]);
